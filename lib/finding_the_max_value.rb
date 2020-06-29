c1290050e7df2a76d394b7d4f5aa4a751c2a6876
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-29 15:37:12 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,18 @@
+# solution 1
 def find_max_value(array)
   max = array.sort 
   max.last 
+end
+
+#solution 2
+def find_max_value(array)
+  count = 0 
+  max_value = -1
+  while count < array.length do
+    if max_value < array[count]
+      max_value = array[count]
+end
+    count += 1 
+  end
+  max_value
 end
